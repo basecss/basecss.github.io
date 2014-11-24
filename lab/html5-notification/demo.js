55f199def8e42305ce517b144897fa0d983bbbe5
basecss <i@basecss.net>
2014-11-24 14:07:19 +0800

    Add HTML5 Notification demo

---
+++ lab/html5-notification/demo.js
@@ -0,0 +1,37 @@
+(function() {
+
+	var demo = document.querySelector('#demo'),
+		notification;
+
+	demo.addEventListener('click', function() {
+
+		if(!('Notification' in window)) {
+			alert('Web Notification is not supported')
+			return
+		}
+
+		Notification.requestPermission(function(permission) {
+
+			if(permission !== 'granted') {
+				return;
+			}
+
+			notification = new Notification('Notification Title', {
+				dir: 'ltr',
+				body: 'This is notification content text',
+				icon: 'http://www.basecss.net/img/avatar.jpg'
+			})
+
+			setTimeout(function() {
+
+				notification.close()
+
+			}, 3000)
+
+		})
+
+	}, false)
+
+
+
+})()
---
+++ lab/html5-notification/sample.js
@@ -0,0 +1,23 @@
+(function() {
+
+	var sample = document.querySelector('#sample')
+
+	sample.addEventListener('click', function() {
+
+		if(!('Notification' in window)) {
+			alert('你的浏览器不支持这个API')
+			return
+		}
+		Notification.requestPermission(function(permission) {
+			var sampleNotification = new Notification('Sample Title', {
+				body: 'Sample body'
+			})
+
+			setTimeout(function() {
+				sampleNotification.close()
+			}, 3000)
+		})
+
+	}, false)
+
+})()
